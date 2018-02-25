a113922884858b1fe2432bb23bef5064c353f48c
Serban <serban.dobroiu@gmail.com>
2018-02-25 19:52:39 +0000

    first simple app

--- Main.elm
+++ Main.elm
@@ -1,13 +1,92 @@
 module Main exposing (..)
 
-import Html
+import Html exposing (..)
 
-add a b =
-    a + b
-    
-result =
-    -- add 1 2 |> add 3
-    add 2 2 |> \a -> a % 2 == 0
+import Html.Attributes exposing (..)
 
+import Html.Events exposing (..)
+
+
+-- MODEL
+
+type alias Model =
+    { calories : Int
+    , input : Int
+    , error : Maybe String
+    }
+
+
+initModel : Model
+initModel = 
+    Model 0 0 Nothing
+
+
+
+-- UPDATE
+
+type Msg 
+    = AddCalorie
+    | Input String
+    | Clear
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        AddCalorie ->
+            { model | calories = model.calories + model.input, input = 0 }
+        
+        Input val ->
+            case String.toInt val of
+                Ok input ->
+                    { model | input = input, error = Nothing }
+                
+                Err err ->
+                    { model | input = 0, error = Just err }
+        
+        Clear ->
+            initModel
+
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    div [] [
+            h3 []
+                [ text ("Total Calories: " ++ (toString model.calories)) ]
+            , input [ type_ "text"
+                , onInput Input
+                , value
+                    (if model.input == 0 then 
+                        ""
+                    else
+                        toString model.input)
+                    ]
+                []
+            , div [] [ text (Maybe.withDefault "" model.error) ]
+            , button
+                [ type_ "button"
+                , onClick AddCalorie
+                ]
+                [ text "Add" ]
+            , button
+                [ type_ "button"
+                , onClick Clear
+                ]
+                [ text "Clear" ]
+            ]
+
+
+
+main : Program Never Model Msg
 main =
-    Html.text (toString result)
+    Html.beginnerProgram
+        { model = initModel
+        , view = view
+        , update = update
+        -- , subscriptions = subscriptions
+        }
+    
+
